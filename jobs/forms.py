1598f882acf466c4b554c0a354476741f42c0fb2
divissha <divissha42@gmail.com>
2026-06-07 14:24:41 +0530

    Added login error messages

--- jobs/forms.py
+++ jobs/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from .models import Application
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class ApplicationForm(forms.ModelForm):
 
@@ -9,4 +11,14 @@ class ApplicationForm(forms.ModelForm):
         fields = [
          
             'resume'
+        ]
+
+class RegisterForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = [
+            'username',
+            'email',
+            'password1',
+            'password2'
         ]
